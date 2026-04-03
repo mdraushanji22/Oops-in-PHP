e84e09e0844cfcf385eb654e5597da0dd290aa5f
mdraushan <mdraushanji22@gmail.com>
2026-04-03 22:38:52 +0530

    Oops in PHP

--- properties.php
+++ properties.php
@@ -7,6 +7,12 @@ class Properties
     {
         return $this->name;
     }
+    function updateName()
+    {
+        $this->name = "Md Raushan Jilani";
+    }
 }
 $p1 = new Properties();
+// echo $p1->name;
+echo $p1->updateName();
 echo $p1->getName();
