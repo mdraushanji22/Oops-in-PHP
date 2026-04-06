6d18579f66bf28a16678498053ccbfc64cdf3839
mdraushan <mdraushanji22@gmail.com>
2026-04-06 23:32:15 +0530

    AccessModifier

---
+++ staticMethod.php
@@ -0,0 +1,18 @@
+<?php
+class greeting
+{
+    // static method
+    public static function welcome()
+    {
+        echo "Hello World!";
+    }
+    public static function getName($name)
+    {
+        echo $name;
+    }
+}
+
+// Call static method directly
+greeting::welcome();
+echo "<br>";
+greeting::getName("Md Raushan Jilani");
