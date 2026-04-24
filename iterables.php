c272829d87b6d5572016e6450c331d0359e9dffe
mdraushan <mdraushanji22@gmail.com>
2026-04-24 22:14:50 +0530

    Iterables in PHP

---
+++ iterables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    function getIterable(): iterable
+    {
+        return ["a", "b", "c"];
+    }
+
+    foreach (getIterable() as $item) {
+        echo $item;
+    }
+    ?>
+
+</body>
+
+</html>
