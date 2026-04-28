22a5854a73ac53f338faf38f7e8e79b24f43170e
mdraushan <mdraushanji22@gmail.com>
2026-04-28 21:37:55 +0530

    constant in oops

--- constant.php
+++ constant.php
@@ -7,15 +7,24 @@
     class Goodbye
     {
         const MESSAGE = "Thank you for the valuable information";
+        const GoodMESSAGE = "Thank you for the Good message";
+
 
         public function bye()
         {
             echo self::MESSAGE;
         }
+        public function Goodbye()
+        {
+            echo self::GoodMESSAGE;
+        }
     }
 
     $goodbye = new Goodbye();
     $goodbye->bye();
+    echo "<br>";
+    $goodbye->Goodbye();
+
     ?>
 
 </body>
